53304ac0475ab3581505b70c8db456ac51b1d8be
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-08-09 08:11:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,8 +36,8 @@ def self.find_or_create_by_name(name)
   self.find_by_name(name) || self.create_by_name(name)
 end
 
-def self.alphebetical(song)
-  @@all.sort_by{|song| song.name}
+def self.alphabetical
+  @@all.sort_by{|x| x.name}
 end
 
 def self.new_from_filename(file_name)

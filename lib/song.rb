f18c87b0b088f4bc5cc6af0cfdfc271b3c49233c
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-08-09 08:09:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,14 +40,14 @@ def self.alphebetical()
   @@all.sort_by{|x| x.name}
 end
 
-def self.new_from_filename(filename)
+def self.new_from_filename(file_name)
    new_song = self.new
     new_song.name = file_name.split(" - ")[1].split(".")[0]
     new_song.artist_name = file_name.split(" - ")[0]
     new_song
 end
 
-def self.create_from_filename(filename)
+def self.create_from_filename(file_name)
   new_song = self.new
     new_song.name = file_name.split(" - ")[1].split(".")[0]
     new_song.artist_name = file_name.split(" - ")[0]

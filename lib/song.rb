5d7fafa0cee92fbb9278ae2273156f7fe4da83f0
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-08-09 07:38:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ end
 
 def self.new_from_filename(filename)
   song = self.new
-  song.name = (name.split(" - ")[1].chomp(".mp3")
+  song.name = (name.split(" - ")[1].chomp(".mp3"))
   song.artist_name =(name.split("-")[0])
   song 
 end

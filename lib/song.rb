04948dadc58a077667d8ba7fe89bc6c5960a9f5d
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-08-09 06:25:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,46 @@ class Song
     self.class.all << self
   end
 
+def self.create
+  song = self.new 
+  self.all << song
+  song 
+end
+
+def self.new_by_name(title)
+  song=self.new 
+  song.name = title 
+  song
+end
+
+def self.create_by_name(title)
+  song = self.create 
+  song.name = title 
+  song 
+end
+
+def self.find_by_name(title)
+  
+end
+
+def self.find_or_create_by_name(title)
+  
+end
+
+def self.alphebetical
+  sorted = self.all.sort_by{|song|song.name}
+  sorted
+end
+
+def self.new_from_filename(filename)
+  
+end
+
+def self.create_from_filename(filename)
+  
+end
+  
+def self.destroy_all
+  self.all.clear
+end
 end
